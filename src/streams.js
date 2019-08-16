22c2caf408e9a9be3c83aedb86a03a145a64c8e2
SilvaEmerson <emersonjose877@gmail.com>
2019-08-16 14:02:16 -0300

    Rename Subject objects

--- src/streams.js
+++ src/streams.js
@@ -6,13 +6,13 @@ const baseCounterObs = new timer(0, 1000).pipe(map(el => el + 1));
 
 export const totalStepsSubject = new Subject();
 
-export const isStoped = new Subject();
+export const isStopedSubject = new Subject();
 
-export const isWalking = new Subject();
+export const isWalkingSubject = new Subject();
 
-export const counterObs = baseCounterObs.pipe(takeUntil(isStoped));
+export const counterObs = baseCounterObs.pipe(takeUntil(isStopedSubject));
 
 export const stopTimeObs = baseCounterObs.pipe(
   map(i => `${Math.floor(i / 60)}m: ${i % 60}s`),
-  takeUntil(isWalking)
+  takeUntil(isWalkingSubject)
 );
