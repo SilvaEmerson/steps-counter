847f7aade43a84cca78f6f0ae83255c922ca370e
SilvaEmerson <emersonjose877@gmail.com>
2019-08-16 07:50:26 -0300

    Updates

---
+++ precache-manifest.4b64b7245f5260098f4de52e33f89edb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80cfd777b23576015bdae735d12efb59",
+    "url": "/steps-counter/index.html"
+  },
+  {
+    "revision": "86e32e9f725652f68601",
+    "url": "/steps-counter/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b87a9049c27bddc0b3e7",
+    "url": "/steps-counter/static/js/2.fe276471.chunk.js"
+  },
+  {
+    "revision": "86e32e9f725652f68601",
+    "url": "/steps-counter/static/js/main.1720abe5.chunk.js"
+  },
+  {
+    "revision": "23b8ad880cd90a5774c2",
+    "url": "/steps-counter/static/js/runtime~main.3acd64e2.js"
+  }
+]);
